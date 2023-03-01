21eb15d0f3829d75308179c31daf0fc2322d16d0
Tanmay Agarwal <tanmay56@student.ubc.ca>
2023-03-01 18:24:53 -0500

    Created panels and added table for panel 2

--- app.R
+++ app.R
@@ -1,93 +1,131 @@
 library(shiny)
-library(shinyWidgets)
+library(bslib)
 library(DT)
-library(ggplot2)
+library(dplyr)
+library(shinythemes)
 
-# Reload when saving the app
+
+
 options(shiny.autoreload = TRUE)
 
+#Loading the Race results data
 race_results <- readr::read_csv("data/formula1_2021season_raceResults.csv")
 race_results$Track <- factor(race_results$Track, levels = unique(race_results$Track))
-race_results <- race_results |>
+
+#Mutating the race results data 
+race_results_updated <- race_results |>
   dplyr::group_by(Driver) |>
   dplyr::mutate(cumpoints = cumsum(Points))
 
-# Define UI for application that highlights rows in a table
-ui <- fluidPage(
-    # Add a slider called "highlight" to determine number of rows to highlight
-    # sliderInput(inputId = 'highlight',
-    #             label = 'Slide to highlight table',
-    #             min = 1,
-    #             max = 5,
-    #             value = 1),
-    # 
-    # # Output is a datatable called 'table1'
-    # dataTableOutput('table1')
-  
-  # checkbox to filter for drivers
-  fluidRow(
-    column(2,
-           checkboxGroupInput(inputId = "driverSelect", 
-                              label = "Select drivers:", 
-                              choices = unique(race_results$Driver), 
-                              selected = c("Lewis Hamilton", "Carlos Sainz")),
-           style="overflow-x: scroll; overflow-y: scroll"
-    ),
-    # display line chart
-    column(8,
-           plotOutput("distPlot"),
-           fluidRow(
-             tags$style(type = "text/css", ".irs-grid-pol.small {height: 0px;}"), # to hide the minor ticks
-             sliderTextInput(inputId = "raceSlider",
-                             label = "Select races", 
-                             choices = unique(race_results$Track), 
-                             selected = c("Bahrain", "Abu Dhabi"),
-                             grid = TRUE, 
-                             from_fixed = TRUE,
-                             width = "100%")
-           )
-    ),
-    column(2,
-           print("This section for races!")
-    )
-  )
+#Loading the lap info data
+laptimes <- readr::read_csv("data/2021_all_laps_info.csv")
+
+# Load race names
+race_table <- race_results |>
+  dplyr::ungroup() |>
+  dplyr::select(Track) |>
+  dplyr::group_by(Track) |>
+  dplyr::distinct() |>
+  dplyr::rename(Race = 'Track')
+
+ui <- navbarPage("Formula 1 Dashboard",
+                 theme = shinytheme("lumen"),
+                 tabPanel("Panel 1",
+                          
+                          # checkbox to filter for drivers
+                          fluidRow(
+                            column(2,
+                                   checkboxGroupInput(inputId = "driverSelect", 
+                                                      label = "Select drivers:", 
+                                                      choices = unique(race_results$Driver), 
+                                                      selected = c("Lewis Hamilton", "Carlos Sainz")),
+                                   style="overflow-x: scroll; overflow-y: scroll"
+                            ),
+                            # display line chart
+                            column(8,
+                                   plotOutput("distPlot"),
+                                   fluidRow(
+                                     tags$style(type = "text/css", ".irs-grid-pol.small {height: 0px;}"), # to hide the minor ticks
+                                     sliderTextInput(inputId = "raceSlider",
+                                                     label = "Select races", 
+                                                     choices = unique(race_results$Track), 
+                                                     selected = c("Bahrain", "Italy"),
+                                                     grid = TRUE, 
+                                                     from_fixed = TRUE,
+                                                     width = "100%")
+                                   )
+                            ),
+                            # Table of Races that interacts with raceSlider
+                            column(2,
+                                   dataTableOutput('Races')
+                            )
+                          )),
+                 tabPanel('Panel 2',
+                          fluidRow(
+                            # Dropdown for grand prix
+                            column(6, selectInput(inputId = 'gp',
+                                                  label = 'Choose Grand Prix',
+                                                  choices = unique(race_results$GP),
+                                                  selected = "Bahrain Grand Prix",
+                            )),
+                            # Dropdown for driver
+                            column(6, selectInput(inputId = 'driver',
+                                                  label = 'Choose Driver',
+                                                  choices = unique(race_results$Driver),
+                                                  selected = "Lewis Hamilton"))
+                          ),
+                          
+                          fluidRow(
+                            # Table output
+                            column(6,
+                                   DT::DTOutput(outputId = 'race_results_table')),
+                            column(6,
+                                   # plotOutput("lap_times_plot")
+                            )
+                            
+                          )
+                 ),
 )
 
-# Define server logic required to make and highlight a table
 server <- function(input, output, session) {
-    # Example data
-    # df <- faithful[1:5,]
-    # 
-    # # match data with color
-    # vals <- reactiveValues(
-    #     eruptions = df[, 1],
-    #     row_color = rep('white', 5)
-    # )
-    # 
-    # # Change row color depending on the number of highlighted rows
-    # observeEvent(input$highlight, {
-    #     vals$eruptions <-
-    #         c(vals$eruptions[1:input$highlight],
-    #           vals$eruptions[input$highlight+1:length(vals$eruptions)])
-    #     vals$row_color <- c(rep('yellow', input$highlight),
-    #                         rep('white', length(vals$eruptions) - input$highlight))
-    # })
-    # 
-    # # draw the table with the specified number of rows highlighted
-    # output$table1 <- renderDataTable({
-    #     datatable(df) |> formatStyle(
-    #         'eruptions', target = 'row',
-    #         backgroundColor = styleEqual(vals$eruptions,
-    #                                      vals$row_color,
-    #                                      default = 'white')
-    #     )
-    # })
+  
+  ##Functions for Panel 1 here
+  # Initialize race names and colors
+  highlight_races <- reactiveValues()
+  highlight_races$races <- as.character(race_table$Race)
+  highlight_races$row_color <- reactive({rep('white', length(highlight_races$races))})
+  
+  
+  # Change row color depending on the slider race selections
+  observeEvent(input$raceSlider, {
+    start_race <- which(highlight_races$races == input$raceSlider[1])
+    end_race <- which(highlight_races$races == input$raceSlider[2])
+    highlight_races$races <- c(highlight_races$races[start_race:end_race],
+                               highlight_races$races[!(highlight_races$races %in% highlight_races$races[start_race:end_race])])
+    highlight_races$row_color <- c(rep('pink', length(highlight_races$races[start_race:end_race])),
+                                   rep('white', length(highlight_races$races) - length(highlight_races$races[start_race:end_race])))
+  })
+  
+  # Create the table with the specified rows highlighted
+  output$Races <- renderDataTable({
+    datatable(race_table, 
+              options = list("pageLength" = 22,
+                             "searching" = FALSE,
+                             "lengthChange"= FALSE)) |> formatStyle(
+                               'Race', target = 'row',
+                               backgroundColor = styleEqual(levels = highlight_races$races,
+                                                            values = highlight_races$row_color,
+                                                            default = "white")
+                             )
+  })
   
   # filter data frame for drivers based on selection
   drivers_plotting <- reactive({
-    race_results |>
+    race_results_updated |>
       dplyr::filter(Driver %in% input$driverSelect)
   })
+  
+  
   # draw the cumulative points line chart
   output$distPlot <- renderPlot({
     ggplot2::ggplot(drivers_plotting(), aes(x = Track, y = cumpoints, group = Driver, color = Driver)) +
@@ -105,7 +143,47 @@ server <- function(input, output, session) {
         legend.position = "top",
       )
   })
+  
+  
+  ##Functions for Panel 2 here
+  # Filter data frame for Grand Prix based on selection
+  filtered_race_results <- reactive(
+    race_results |> 
+      dplyr::filter(GP == input$gp) |> 
+      dplyr::select(-GP) 
+  )
+  
+  # Filter the data frame for lap times based on selection
+  filtered_laptimes <- reactive(laptimes |> 
+                                  dplyr::filter(name == input$driver, GP == input$gp)  |> 
+                                  mutate(lap_times_sec = 0.001*lap_time_ms))
+  
+  # Render the race results table
+  output$race_results_table <- DT::renderDT({
+    
+    
+    datatable(filtered_race_results())
+    
+  })
+  
+  # Draw the lap times for every driver
+  output$lap_times_plot <- renderPlot({
+    ggplot2::ggplot(filtered_laptimes(), aes(x = lap, y = lap_times_sec)) +
+      ggplot2::geom_line() +
+      ggplot2::geom_point() +
+      ggplot2::labs(x = "Lap number", y = "Lap times(in sec)") +
+      ggplot2::ggtitle("Lap times For Driver") +
+      ggplot2::theme(
+        plot.title = element_text(size = 31, face = "bold"),
+        axis.text.x = element_text(size = 10, angle = 20, vjust = 0.6),
+        axis.text.y = element_text(size = 10),
+        axis.title = element_text(size = 15, face = "bold"),
+        legend.text = element_text(size = 10, face = "bold"),
+        legend.title = element_blank(),
+        legend.position = "top")
+    
+  })
+  
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
